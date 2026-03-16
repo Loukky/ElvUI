86d413cf323cbf585fc996bf1d157ced4092df66
Simpy <kodi03466@hotmail.com>
2026-03-16 18:42:23 -0400

    Filter: arrange List a bit more

--- ElvUI/Game/Shared/Filters/Filters.lua
+++ ElvUI/Game/Shared/Filters/Filters.lua
@@ -6,11 +6,11 @@ local unpack = unpack
 E.Filters = {}
 E.Filters.Included = {}
 
-E.Filters.List = function(priority, enable, stack)
+E.Filters.List = function(priority, enable, stackThreshold)
 	return {
-		enable = (enable == nil and true) or enable,
 		priority = priority or 0,
-		stackThreshold = stack or 0
+		enable = (enable == nil and true) or enable,
+		stackThreshold = stackThreshold or 0
 	}
 end
 
