005f773db0442fab632281cf9a68110664eaff96
Simpy <kodi03466@hotmail.com>
2026-04-21 21:31:03 -0400

    DT: versatility ignore GetVersatilityBonus values

--- ElvUI/Game/Mainline/DataTexts/Versatility.lua
+++ ElvUI/Game/Mainline/DataTexts/Versatility.lua
@@ -4,7 +4,6 @@ local DT = E:GetModule('DataTexts')
 local format, strjoin = format, strjoin
 local GetCombatRating = GetCombatRating
 local GetCombatRatingBonus = GetCombatRatingBonus
-local GetVersatilityBonus = GetVersatilityBonus
 local BreakUpLargeNumbers = BreakUpLargeNumbers
 
 local CR_VERSATILITY_DAMAGE_DONE = CR_VERSATILITY_DAMAGE_DONE
@@ -18,24 +17,21 @@ local function OnEnter()
 	DT.tooltip:ClearLines()
 
 	local versatility = GetCombatRating(CR_VERSATILITY_DAMAGE_DONE)
-	local damageDone = GetVersatilityBonus(CR_VERSATILITY_DAMAGE_DONE)
-	local damageTaken = GetVersatilityBonus(CR_VERSATILITY_DAMAGE_TAKEN)
 	local bonusDamage = GetCombatRatingBonus(CR_VERSATILITY_DAMAGE_DONE)
 	local bonusTaken = GetCombatRatingBonus(CR_VERSATILITY_DAMAGE_TAKEN)
 
-	local tooltip = format('%s: %s [%.2f%% (+ %.2f%%) / %.2f%% (+ %.2f%%)]', STAT_VERSATILITY, BreakUpLargeNumbers(versatility), damageDone, bonusDamage, damageTaken, bonusTaken)
+	local tooltip = format('%s: %s [%.2f%% / %.2f%%]', STAT_VERSATILITY, BreakUpLargeNumbers(versatility), bonusDamage, bonusTaken)
 	DT.tooltip:AddLine(tooltip, nil, nil, nil, true)
 	DT.tooltip:Show()
 end
 
 local function OnEvent(self)
 	local bonusDamage = GetCombatRatingBonus(CR_VERSATILITY_DAMAGE_DONE)
-	local damageDone = GetVersatilityBonus(CR_VERSATILITY_DAMAGE_DONE)
 
 	if db.NoLabel then
-		self.text:SetFormattedText(displayString, damageDone, bonusDamage)
+		self.text:SetFormattedText(displayString, bonusDamage)
 	else
-		self.text:SetFormattedText(displayString, db.Label ~= '' and db.Label or STAT_VERSATILITY, damageDone, bonusDamage)
+		self.text:SetFormattedText(displayString, db.Label ~= '' and db.Label or STAT_VERSATILITY, bonusDamage)
 	end
 end
 
@@ -44,7 +40,7 @@ local function ApplySettings(self, hex)
 		db = E.global.datatexts.settings[self.name]
 	end
 
-	displayString = strjoin('', db.NoLabel and '' or '%s: ', hex, '%.'..db.decimalLength..'f%% (+%.'..db.decimalLength..'f%%|r')
+	displayString = strjoin('', db.NoLabel and '' or '%s: ', hex, '%.'..db.decimalLength..'f%%|r')
 end
 
 DT:RegisterDatatext('Versatility', STAT_CATEGORY_ENHANCEMENTS, { 'UNIT_STATS', 'UNIT_AURA', 'ACTIVE_TALENT_GROUP_CHANGED', 'PLAYER_TALENT_UPDATE', 'PLAYER_DAMAGE_DONE_MODS' }, OnEvent, nil, nil, OnEnter, nil, STAT_VERSATILITY, nil, ApplySettings)
